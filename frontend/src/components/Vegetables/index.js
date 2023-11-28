7aa758a1fdbbc74f84323649d6645e629eab782b
willmchristensen <willmchristensen@gmail.com>
2023-11-27 16:17:45 -0800

    css

--- frontend/src/components/Vegetables/index.js
+++ frontend/src/components/Vegetables/index.js
@@ -3,16 +3,24 @@ import './Vegetables.css';
 
 let veggies = [
     {   title:'Tomato',
-        price:5.55, description:'descriptiondescriptiondesdescription'
+        price:5.55, 
+        description:'descriptiondescriptiondesdescription',
+        
     },
     {   title:'Lettuce',
-        price:4.55, description:'descriptiondescriptiondesdescription'
+        price:4.55, 
+        description:'descriptiondescriptiondesdescription',
+        
     },
     {   title:'Squash',
-        price:3.55, description:'descriptiondescriptiondesdescription'
+        price:3.55, 
+        description:'descriptiondescriptiondesdescription',
+        
     },
     {   title:'Broccoli',
-        price:6.55, description:'descriptiondescriptiondesdescription'
+        price:6.55, 
+        description:'descriptiondescriptiondesdescription',
+        
     },
 ];
 function Vegetables({handleClick}){
